15b6d1fd8afbe9ee50e61a84d2a6a3bc17f4afdc
evanwitort <evanwitort2018@u.northwestern.edu>
2016-01-20 14:43:17 -0600

    Commit

--- src/lab3.cpp
+++ src/lab3.cpp
@@ -8,6 +8,25 @@
  * functions to run.
  */
 int main() {
+    double inch;
+    vector <double> inches;
+   while (cin>>inch && inch != 0) {
+       inches.push_back(inch);
+   }
+
+    for (int i = 0; i < inches.size(); i++ ) {
+        cout<< inches[i];
+
+    }
+
+
+ if (cin.fail()) {
+     cout << "You must enter a number"
+ }
+else {
+     cout<<"\nIn Feet == "<<inch*30.48 << " ft"  // if double is inputted, multiply double by conversion factor
+<<'\n';
+ }
 
     return 0; // Lets the operating system know everything ran successfully.
 }
